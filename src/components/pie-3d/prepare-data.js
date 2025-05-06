ad7e534eafc63f395af9d85656736631bf250ce4
Dmitry Koroliov <d.koroliov@gmail.com>
2025-05-06 23:36:26 +0300

    Rotate pie around its inner center
    We used to rotate the pie around the center of its Heads side. This led
    to the pie being shifted (for a demonstration one can uncomment the
    drawDataOnCanvas() call in the committed test) and compare the new
    expected data and the old one.
    
    Now it's rotated around its inner, true center.

--- src/components/pie-3d/prepare-data.js
+++ src/components/pie-3d/prepare-data.js
@@ -75,35 +75,35 @@ export function prepareData(arg: AddComponentPie3dArgument): PieData {
     const circleRadius = ops.radiusPx;
     const centerX = ops.centerXPx;
     const centerY = ops.centerYPx;
-    const thickness = ops.thicknessPx;
+    const halfThickness = ops.thicknessPx / 2;
 
     pieData.pointTopHeads[0] = centerX;
     pieData.pointTopHeads[1] = centerY - circleRadius;
-    pieData.pointTopHeads[2] = 0;
+    pieData.pointTopHeads[2] = -halfThickness;
 
     pieData.centerHeads[0] = centerX;
     pieData.centerHeads[1] = centerY;
-    pieData.centerHeads[2] = 0;
+    pieData.centerHeads[2] = -halfThickness;
 
     pieData.centerTails[0] = centerX;
     pieData.centerTails[1] = centerY;
-    pieData.centerTails[2] = thickness;
+    pieData.centerTails[2] = halfThickness;
 
     pieData.edgeLeft.pointHeads[0] = centerX - circleRadius;
     pieData.edgeLeft.pointHeads[1] = centerY;
-    pieData.edgeLeft.pointHeads[2] = 0;
+    pieData.edgeLeft.pointHeads[2] = -halfThickness;
 
     pieData.edgeLeft.pointTails[0] = centerX - circleRadius;
     pieData.edgeLeft.pointTails[1] = centerY;
-    pieData.edgeLeft.pointTails[2] = thickness;
+    pieData.edgeLeft.pointTails[2] = halfThickness;
 
     pieData.edgeRight.pointHeads[0] = centerX + circleRadius;
     pieData.edgeRight.pointHeads[1] = centerY;
-    pieData.edgeRight.pointHeads[2] = 0;
+    pieData.edgeRight.pointHeads[2] = -halfThickness;
 
     pieData.edgeRight.pointTails[0] = centerX + circleRadius;
     pieData.edgeRight.pointTails[1] = centerY;
-    pieData.edgeRight.pointTails[2] = thickness;
+    pieData.edgeRight.pointTails[2] = halfThickness;
 
     let startAngle = ops.startAtDeg / 180 * Math.PI;
     let { expectToPassRightEdge, expectToPassLeftEdge, } =
@@ -114,11 +114,11 @@ export function prepareData(arg: AddComponentPie3dArgument): PieData {
       const x = Math.cos(startAngle) * circleRadius + centerX;
       sd.startPointHeads[0] = x;
       sd.startPointHeads[1] = y;
-      sd.startPointHeads[2] = 0;
+      sd.startPointHeads[2] = -halfThickness;
 
       sd.startPointTails[0] = x;
       sd.startPointTails[1] = y;
-      sd.startPointTails[2] = thickness;
+      sd.startPointTails[2] = halfThickness;
 
       const endAngle = sd.value / totalValue * Math.PI * 2;
       startAngle += endAngle;
