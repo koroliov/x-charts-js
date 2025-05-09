779df6984900a499d3093c79510225876f7e16cd
Dmitry Koroliov <d.koroliov@gmail.com>
2025-05-09 20:06:04 +0300

    Change getAngleCounterClockwise() to clockwise
    This seems to be the way the value will be used in .ellipse() drawings,
    so will be more convenient to have getAngleClockwise().

--- src/test-unit/utils/math.test.js
+++ src/test-unit/utils/math.test.js
@@ -1,56 +1,56 @@
 //@flow strict
 //$FlowFixMe[cannot-resolve-module]
 import tp from 'tape';
-import { getAngleCounterClockwise } from '../../utils/math.js';
+import { getAngleClockwise } from '../../utils/math.js';
 
 //test data angles are measured counter clockwise
 tp.test((t) => {
   //angle 0
-  const actual = getAngleCounterClockwise({
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [1, 0, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, -0);
+  t.equal(actual, 0);
   t.end();
 });
 
 tp.test((t) => {
-  //angle 360 - 45deg
-  const actual = getAngleCounterClockwise({
+  //angle 45deg
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [1, 1, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, 2 * Math.PI - Math.PI / 4);
+  t.equal(actual, Math.PI / 4);
   t.end();
 });
 
 tp.test((t) => {
-  //angle 270deg
-  const actual = getAngleCounterClockwise({
+  //angle 90deg
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [0, 1, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, 2 * Math.PI - Math.PI / 2);
+  t.equal(actual, Math.PI / 2);
   t.end();
 });
 
 tp.test((t) => {
-  //angle 225deg
-  const actual = getAngleCounterClockwise({
+  //angle 135deg
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [-1, 1, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, Math.PI + Math.PI / 4);
+  t.equal(actual, 3 * Math.PI / 4);
   t.end();
 });
 
 tp.test((t) => {
   //angle 180deg
-  const actual = getAngleCounterClockwise({
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [-1, 0, 0],
     centerPoint: [0, 0, 0],
@@ -60,34 +60,34 @@ tp.test((t) => {
 });
 
 tp.test((t) => {
-  //angle 135deg
-  const actual = getAngleCounterClockwise({
+  //angle 225deg
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [-1, -1, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, Math.PI - Math.PI / 4);
+  t.equal(actual, Math.PI + Math.PI / 4);
   t.end();
 });
 
 tp.test((t) => {
-  //angle 90deg
-  const actual = getAngleCounterClockwise({
+  //angle 270deg
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [0, -1, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, Math.PI / 2);
+  t.equal(actual, Math.PI / 2 * 3);
   t.end();
 });
 
 tp.test((t) => {
-  //angle 45deg
-  const actual = getAngleCounterClockwise({
+  //angle 315deg
+  const actual = getAngleClockwise({
     startPoint: [1, 0, 0],
     endPoint: [1, -1, 0],
     centerPoint: [0, 0, 0],
   });
-  t.equal(actual, Math.PI / 4);
+  t.equal(actual, 2 * Math.PI / 8 * 7);
   t.end();
 });
--- src/utils/math.js
+++ src/utils/math.js
@@ -1,7 +1,7 @@
 //@flow strict
 import type { Point, } from '../types.js';
 
-export function getAngleCounterClockwise({ startPoint, endPoint, centerPoint, }:
+export function getAngleClockwise({ startPoint, endPoint, centerPoint, }:
   { startPoint: Point, endPoint: Point, centerPoint: Point, }): number {
   const dotProduct =
     (startPoint[0] - centerPoint[0]) * (endPoint[0] - centerPoint[0]) +
@@ -11,10 +11,13 @@ export function getAngleCounterClockwise({ startPoint, endPoint, centerPoint, }:
     (startPoint[1] - centerPoint[1]) * (endPoint[0] - centerPoint[0]);
   const atan2 = Math.atan2(crossProduct, dotProduct);
 
-  if (atan2 > 0) {
-    return 2 * Math.PI - atan2;
+  if (atan2 === 0) {
+    return Math.abs(0);
   }
-  return -atan2;
+  if (atan2 < 0) {
+    return 2 * Math.PI + atan2;
+  }
+  return atan2;
 }
 
 export function calculateDistance({ pointStart, pointEnd, }:
