3169d19d2910b474ab6ffeeefe563df885f62b8f
Dmitry Koroliov <d.koroliov@gmail.com>
2025-05-22 00:08:35 +0300

    Add test for pie-3d draw() method
    The test when the left edge lies exactly on the boundary between 2
    slices.

--- src/test-unit/components/pie-3d/draw.test.js
+++ src/test-unit/components/pie-3d/draw.test.js
@@ -49,3 +49,45 @@ tp.test((t) => {
     t.end();
   });
 });
+
+tp.test((t) => {
+  const testName = '0002-left-edge-on-slices-boundary';
+  const arg = {
+    type: 'pie-3d',
+    zIndex: '1',
+    options: {
+      thicknessPx: 50,
+      radiusPx: 200,
+      centerXPx: 235,
+      centerYPx: 110,
+      startAtDeg: 90,
+      rotationAroundCenterXAxisDeg: 70,
+      rotationAroundCenterZAxisDeg: 0,
+    },
+    data: [
+      { value: 25, meta: { color: '#f2b5f6' /* pinkish */, }, },
+      { value: 25, meta: { color: '#00fff5' /* cyan */, }, },
+      { value: 25, meta: { color: '#feed00' /* yellow */, }, },
+      { value: 25, meta: { color: '#ff0010' /* red */, }, },
+    ],
+  };
+  const { ctx, canvas, } =
+    createCanvasContext2d({ w: 470, h: 220, fillStyle: 'white', });
+  draw({ ctx, addComponentArg: arg, });
+
+  canvas.toBuffer(async (err: null | Error, buff: Buffer) => {
+    //await writeCanvasToTestDiffDir({
+    //  canvas: ctx.canvas,
+    //  fileNameRelative: './test/diff/current.png',
+    //});
+    const equal = await compareWithLooksSame({
+      buffer: buff,
+      expectedFileNameRelative:
+        `./test/unit-permanent/components/pie-3d/draw/${ testName }.png`,
+      highlightColor: '#000000',
+      diffFileNameRelativeOnError: `./test/diff/${ testName }.png`,
+    });
+    t.ok(equal);
+    t.end();
+  });
+});
