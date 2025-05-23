0a3ba043d5b7b039d522df9f8b44cb71d9296ceb
Dmitry Koroliov <d.koroliov@gmail.com>
2025-05-23 23:02:13 +0300

    Implement cases when tails visible in pie-3d
    Turned out to be quite simple, so didn't feel any need for any tests.

--- src/components/pie-3d/prepare-rim-slices-data.js
+++ src/components/pie-3d/prepare-rim-slices-data.js
@@ -58,51 +58,51 @@ export function prepareRimSlicesData(pieData: PieData): RimSlicesData {
       rimSlicesData.push(sd);
     }
   } else {
-    //for (let i = startSliceIndex; i <= endSliceIndex; i++) {
-    //  const sd: RimSlicesData[0] = {
-    //    color: pieData.slices[i].color,
-    //    pointStartOnVisibleFace: i === startSliceIndex ?
-    //      pieData.edgeLeft.pointTails : pieData.slices[i].startPointTails,
-    //    pointStartOnInvisibleFace: i === startSliceIndex ?
-    //      pieData.edgeLeft.pointHeads : pieData.slices[i].startPointHeads,
-    //    pointEndOnInvisibleFace: i === endSliceIndex ?
-    //      pieData.edgeRight.pointHeads : pieData.slices[i].endPointHeads,
-    //    pointEndOnVisibleFace: i === endSliceIndex ?
-    //      pieData.edgeRight.pointTails : pieData.slices[i].endPointTails,
+    for (let j = 0; j <= indicesToPassThru.length - 1; j++) {
+      const i = indicesToPassThru[j];
+      const sd: RimSlicesData[0] = {
+        color: pieData.slices[i].color,
+        pointStartOnVisibleFace: i === startSliceIndex ?
+          pieData.edgeLeft.pointTails : pieData.slices[i].startPointTails,
+        pointStartOnInvisibleFace: i === startSliceIndex ?
+          pieData.edgeLeft.pointHeads : pieData.slices[i].startPointHeads,
+        pointEndOnInvisibleFace: i === endSliceIndex ?
+          pieData.edgeRight.pointHeads : pieData.slices[i].endPointHeads,
+        pointEndOnVisibleFace: i === endSliceIndex ?
+          pieData.edgeRight.pointTails : pieData.slices[i].endPointTails,
 
-    //    ellipseArgumentsOnVisibleFace: {
-    //      centerX: pieData.centerTails[0],
-    //      centerY: pieData.centerTails[1],
-    //      radiusX: pieData.someEllipseMethodArgs.radiusX,
-    //      radiusY: pieData.someEllipseMethodArgs.radiusY,
-    //      axesRotationCounterClockwise:
-    //        pieData.someEllipseMethodArgs.axesRotationCounterClockwise,
-    //      angleStart: i === startSliceIndex ?
-    //        Math.PI :
-    //        pieData.slices[i].startAngleOnEllipseClockwise,
-    //      angleEnd: i === endSliceIndex ?
-    //        Math.PI * 2 : pieData.slices[i].endAngleOnEllipseClockwise,
-    //      isCounterClockwise: true,
-    //    },
+        ellipseArgumentsOnInvisibleFace: {
+          centerX: pieData.centerHeads[0],
+          centerY: pieData.centerHeads[1],
+          radiusX: pieData.someEllipseMethodArgs.radiusX,
+          radiusY: pieData.someEllipseMethodArgs.radiusY,
+          axesRotationCounterClockwise:
+            pieData.someEllipseMethodArgs.axesRotationCounterClockwise,
+          angleStart: i === startSliceIndex ?
+            Math.PI :
+            pieData.slices[i].startAngleOnEllipseClockwise,
+          angleEnd: i === endSliceIndex ?
+            Math.PI * 2 : pieData.slices[i].endAngleOnEllipseClockwise,
+          isCounterClockwise: false,
+        },
 
-    //    ellipseArgumentsOnInvisibleFace: {
-    //      centerX: pieData.centerHeads[0],
-    //      centerY: pieData.centerHeads[1],
-    //      radiusX: pieData.someEllipseMethodArgs.radiusX,
-    //      radiusY: pieData.someEllipseMethodArgs.radiusY,
-    //      axesRotationCounterClockwise:
-    //        pieData.someEllipseMethodArgs.axesRotationCounterClockwise,
-    //      angleStart: i === endSliceIndex ?
-    //        0 :
-    //        pieData.slices[i].endAngleOnEllipseClockwise,
-    //      angleEnd: i === startSliceIndex ?
-    //        Math.PI * 2 : pieData.slices[i].startAngleOnEllipseClockwise,
-    //      isCounterClockwise: false,
-    //    },
-
-    //  };
-    //  rimSlicesData.push(sd);
-    //}
+        ellipseArgumentsOnVisibleFace: {
+          centerX: pieData.centerTails[0],
+          centerY: pieData.centerTails[1],
+          radiusX: pieData.someEllipseMethodArgs.radiusX,
+          radiusY: pieData.someEllipseMethodArgs.radiusY,
+          axesRotationCounterClockwise:
+            pieData.someEllipseMethodArgs.axesRotationCounterClockwise,
+          angleStart: i === endSliceIndex ?
+            0 :
+            pieData.slices[i].endAngleOnEllipseClockwise,
+          angleEnd: i === startSliceIndex ?
+            Math.PI : pieData.slices[i].startAngleOnEllipseClockwise,
+          isCounterClockwise: true,
+        },
+      };
+      rimSlicesData.push(sd);
+    }
   }
   return rimSlicesData;
 
