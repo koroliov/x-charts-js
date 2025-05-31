184f16170e80e05bf18dc5a52815a430d6cc0485
Dmitry Koroliov <d.koroliov@gmail.com>
2025-05-31 22:25:43 +0300

    Make math util to work c-clockwise & clockwise
    The tests are amended, but other places of use are not being committed
    now.

--- src/test-unit/utils/math.test.js
+++ src/test-unit/utils/math.test.js
@@ -1,15 +1,16 @@
 //@flow strict
 //$FlowFixMe[cannot-resolve-module]
 import tp from 'tape';
-import { getAngleClockwise, } from '../../utils/math.js';
+import { getAngleBetweenTwoPoints, } from '../../utils/math.js';
 
-//test data angles are measured counter clockwise
+//clockwise angles
 tp.test((t) => {
   //angle 0
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [1, 0, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, 0);
   t.end();
@@ -17,10 +18,11 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 45deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [1, 1, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, Math.PI / 4);
   t.end();
@@ -28,10 +30,11 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 90deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [0, 1, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, Math.PI / 2);
   t.end();
@@ -39,10 +42,11 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 135deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [-1, 1, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, 3 * Math.PI / 4);
   t.end();
@@ -50,10 +54,11 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 180deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [-1, 0, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, Math.PI);
   t.end();
@@ -61,10 +66,11 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 225deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [-1, -1, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, Math.PI + Math.PI / 4);
   t.end();
@@ -72,10 +78,11 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 270deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [0, -1, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, Math.PI / 2 * 3);
   t.end();
@@ -83,11 +90,109 @@ tp.test((t) => {
 
 tp.test((t) => {
   //angle 315deg
-  const actual = getAngleClockwise({
+  const actual = getAngleBetweenTwoPoints({
     startPoint: [1, 0, 0],
     endPoint: [1, -1, 0],
     centerPoint: [0, 0, 0],
+    isCounterClockwise: false,
   });
   t.equal(actual, 2 * Math.PI / 8 * 7);
+  t.end();
+});
+
+//counter clockwise angles
+tp.test((t) => {
+  //angle 0
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [1, 0, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, 0);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 315deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [1, 1, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, 2 * Math.PI - Math.PI / 4);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 270deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [0, 1, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, 3 / 2 * Math.PI);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 225deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [-1, 1, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, 5 / 4 * Math.PI);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 180deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [-1, 0, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, Math.PI);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 135deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [-1, -1, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, 3 / 4 * Math.PI);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 90deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [0, -1, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, Math.PI / 2);
+  t.end();
+});
+
+tp.test((t) => {
+  //angle 45deg
+  const actual = getAngleBetweenTwoPoints({
+    startPoint: [1, 0, 0],
+    endPoint: [1, -1, 0],
+    centerPoint: [0, 0, 0],
+    isCounterClockwise: true,
+  });
+  t.equal(actual, 1 / 4 * Math.PI);
   t.end();
 });
