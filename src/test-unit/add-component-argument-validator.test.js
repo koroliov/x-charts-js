e64974c945fc10d17d2cd515e99b835647c92834
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-15 12:39:34 +0300

    Add tests for .add() arg validation
    The tests are heavily stuffed with various Flow error suppressions. I
    need them, b/c the tests cover cases, when it will be executed in pure
    JS, no Flow checks. It seems to be simply impossible to check those
    cases w/o suppressing the errors.

--- src/test-unit/add-component-argument-validator.test.js
+++ src/test-unit/add-component-argument-validator.test.js
@@ -1,6 +1,7 @@
 //@flow strict
 //$FlowFixMe[cannot-resolve-module]
 import tp from 'tape';
+import type { AddComponentArgument, } from '../types.js';
 import { validate, } from '../add-component-argument-validator.js';
 
 tp.test('valid argument case', (t) => {
@@ -24,11 +25,13 @@ tp.test('valid argument case', (t) => {
   const expected = '';
 
   const actual = validate(addComponentArg);
-  t.equal(actual, expected);
+  t.ok(actual === expected);
   t.end();
 });
 
-tp.test.skip('missing type property', (t) => {
+tp.test('missing type property and present on Object.protype', (t) => {
+  //$FlowFixMe[prop-missing]
+  Object.prototype.type = 'pie-3d';
   const addComponentArg = {
     zIndex: '1',
     options: {
@@ -45,10 +48,48 @@ tp.test.skip('missing type property', (t) => {
       { value: 25, meta: { color: '#37ff00' /* green */, }, },
     ],
   };
-  const expected = '';
+  const expected = [
+    'ERR_X_CHARTS_MISSING_PROP_IN_ADD_METHOD_ARG:',
+    "Property 'type' is missing on the provided argument",
+    'to the .add() method (JSON stringified):',
+  ].join('\n');
 
   //$FlowFixMe[prop-missing] we have to test invalid arguments
   const actual = validate(addComponentArg);
-  t.equal(actual, expected);
+  //$FlowFixMe[prop-missing]
+  delete Object.prototype.type;
+  t.ok(actual.startsWith(expected));
+  t.end();
+});
+
+tp.test('missing type property with proto null', (t) => {
+  //$FlowFixMe[incompatible-type]
+  //$FlowFixMe[prop-missing]
+  let addComponentArg: AddComponentArgument = Object.create(null);
+  //$FlowFixMe[cannot-write]
+  addComponentArg = Object.assign(addComponentArg, {
+    zIndex: '1',
+    options: {
+      thicknessPx: 50,
+      radiusPx: 150,
+      centerXPx: 300,
+      centerYPx: 250,
+      startAtDeg: 20,
+      rotationAroundCenterXAxisDeg: 60,
+      rotationAroundCenterZAxisDeg: 45,
+    },
+    data: [
+      { value: 40, meta: { color: '#ff0000' /* red */, }, },
+      { value: 25, meta: { color: '#37ff00' /* green */, }, },
+    ],
+  });
+  const expected = [
+    'ERR_X_CHARTS_MISSING_PROP_IN_ADD_METHOD_ARG:',
+    "Property 'type' is missing on the provided argument",
+    'to the .add() method (JSON stringified):',
+  ].join('\n');
+
+  const actual = validate(addComponentArg);
+  t.ok(actual.startsWith(expected));
   t.end();
 });
