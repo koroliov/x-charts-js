3f2eca187d1b64c270a83243c9665c28a794564b
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-15 20:47:23 +0300

    Save progress before interface change to JSON
    The decision has been made to rework the user facing interface, and make
    the XCharts constructor as well as its instances methods to accept a
    JSON string instead of objects.
    
    Advantages of this is that we will greatly simplify the validation,
    since we will have to bother only with presence/absence of properties
    and the correctness of their values. No need to check where the value
    came from (a prototype or an own property), no getters/setters, no
    references in the argument, e.g. if data[0] is the same ref as data[3]
    etc. This should greatly simplify the validation and reduce potential
    use problems.

--- src/components/pie-3d/add-component-argument-validator.js
+++ src/components/pie-3d/add-component-argument-validator.js
@@ -1,13 +1,23 @@
 //@flow strict
 import type { AddComponentArgument, } from '../../types.js';
 import type { AddComponentPie3dArgument, } from './types.js';
+import {
+  handledPropsSet as topLevelHandledPropsSet,
+} from '../../add-component-argument-validator.js';
 
-export function validateAddComponentArgumentExternal(arg: AddComponentArgument):
+export function validate(arg: AddComponentArgument):
   string {
   const a: AddComponentPie3dArgument =
     //$FlowFixMe[incompatible-cast] See commit message
     //$FlowFixMe[incompatible-variance] See commit message
     //$FlowFixMe[incompatible-indexer] See commit message
     (arg: AddComponentPie3dArgument);
+  //take props from the mapper
+  //take handled props -> set
+  //take props from the arg -> set, exclude handled props from set
+  //for each prop from mapper:
+    //validate
+    //remove from arg props set
+  //check extra remaining props
   return '';
 }
