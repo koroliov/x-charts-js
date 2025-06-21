2f399bde8fd2b1e8f748c1e1c580881ef1036cb9
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-21 10:08:42 +0300

    Add 2 more passing tests

--- src/test-unit/components/pie-3d/add-component-argument-validator.test.js
+++ src/test-unit/components/pie-3d/add-component-argument-validator.test.js
@@ -394,6 +394,40 @@ tp.test('prop is missing', (t) => {
   t.end();
 });
 
+tp.test('prop is not object', (t) => {
+  const addComponentArg = {
+    type: 'pie-3d',
+    zIndex: '1',
+    options: {
+      thicknessPx: 50,
+      radiusPx: 150,
+      centerXPx: 300,
+      centerYPx: 250,
+      startAtDeg: 20,
+      rotationAroundCenterXAxisDeg: 60,
+      rotationAroundCenterZAxisDeg: 45,
+    },
+    data: [
+      { value: 40, meta: { color: '#ff0000' /* red */, }, },
+      { value: 35, meta: true, },
+    ],
+  };
+  const propsToCheck = new Set([
+    'options',
+    'data',
+  ]);
+
+  const expected = [
+    'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
+    'Component pie-3d -> data -> 1 -> meta:',
+    "  must be an object",
+  ].join('\n');
+
+  const actual = validate(propsToCheck, addComponentArg);
+  t.equal(actual, expected);
+  t.end();
+});
+
 tp.test('value is not valid', (t) => {
   const addComponentArg = {
     type: 'pie-3d',
@@ -421,6 +455,40 @@ tp.test('value is not valid', (t) => {
     'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
     'Component pie-3d -> data -> 1 -> value:',
     '  value must be a positive, finite number',
+  ].join('\n');
+
+  const actual = validate(propsToCheck, addComponentArg);
+  t.equal(actual, expected);
+  t.end();
+});
+
+tp.test('prop is missing on level 1', (t) => {
+  const addComponentArg = {
+    type: 'pie-3d',
+    zIndex: '1',
+    options: {
+      thicknessPx: 50,
+      radiusPx: 150,
+      centerXPx: 300,
+      centerYPx: 250,
+      startAtDeg: 20,
+      rotationAroundCenterXAxisDeg: 60,
+      rotationAroundCenterZAxisDeg: 45,
+    },
+    data: [
+      { value: 40, meta: { color: '#ff0000' /* red */, }, },
+      { value: 35, meta: { /*color: '#000aff' * blue ,*/ }, },
+    ],
+  };
+  const propsToCheck = new Set([
+    'options',
+    'data',
+  ]);
+
+  const expected = [
+    'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
+    'Component pie-3d -> data -> 1 -> meta:',
+    "  missing properties: color",
   ].join('\n');
 
   const actual = validate(propsToCheck, addComponentArg);
