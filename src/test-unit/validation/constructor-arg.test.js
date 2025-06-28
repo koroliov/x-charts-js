845ce478e06436f524c1cfe6d14fbe666b1c7e72
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-28 17:02:05 +0300

    Finish validation module w/o some tests
    The tests will be added next.

--- src/test-unit/validation/constructor-arg.test.js
+++ src/test-unit/validation/constructor-arg.test.js
@@ -22,3 +22,63 @@ tp.test('valid argument case', (t) => {
   t.deepEqual(actual, expected);
   t.end();
 });
+
+tp.test('no arguments', (t) => {
+  const ContainerDiv = class ContainerDiv {  }
+  const constructorArg: Array<mixed> = [
+  ];
+  //In tests it's acceptable
+  //$FlowFixMe[incompatible-call]
+  const dict = getDictionary(ContainerDiv);
+  const expected = [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'The new XCharts() constructor expects a single argument',
+  ].join('\n');
+
+  const actual = validate(dict, constructorArg);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+tp.test('extra arguments', (t) => {
+  const ContainerDiv = class ContainerDiv {  }
+  const constructorArg = [
+    {
+      containerDiv: new ContainerDiv(),
+      options: {
+        backgroundColor: '#ffffff' /* white */,
+      },
+    },
+    'foo',
+  ];
+  //In tests it's acceptable
+  //$FlowFixMe[incompatible-call]
+  const dict = getDictionary(ContainerDiv);
+  const expected = [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'The new XCharts() constructor expects a single argument',
+  ].join('\n');
+
+  const actual = validate(dict, constructorArg);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+tp.test('argument is not object', (t) => {
+  const ContainerDiv = class ContainerDiv {  }
+  const constructorArg = [
+    'foo',
+  ];
+  //In tests it's acceptable
+  //$FlowFixMe[incompatible-call]
+  const dict = getDictionary(ContainerDiv);
+  const expected = [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'Argument to the new XCharts() constructor must be an object',
+    'e.g. {  }, Object.create(null)',
+  ].join('\n');
+
+  const actual = validate(dict, constructorArg);
+  t.deepEqual(actual, expected);
+  t.end();
+});
--- src/validation/constructor-arg.js
+++ src/validation/constructor-arg.js
@@ -5,11 +5,57 @@ import { validate as validateByDictionary, } from './by-dictionary.js';
 
 export function validate(dict: ValidationDictionary,
   allAddComponentArgs: Array<mixed>): string {
-  return '';
+  if (allAddComponentArgs.length !== 1) {
+    return generateWrongNumberOfArgumentsErrorReturnValue();
+  }
+  if (!isObject(allAddComponentArgs[0])) {
+    return generateNotObjectArgumentErrorReturnValue();
+  }
+  //the above call of isObject() is supposed to guarantee, that it's an object.
+  //$FlowFixMe[incompatible-type]
+  const arg: { ... } = allAddComponentArgs[0];
+  return validateByDictionary({
+    errorCode: 'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG',
+    topLevelPropName: 'new XCharts() argument',
+    ignorePropsSet: new Set(),
+    dictionary: dict,
+    value: arg,
+  });
 };
 
 export function getDictionary(containerClass: Class<HTMLDivElement>):
   ValidationDictionary {
   return {
+    containerDiv(val) {
+      return (val instanceof containerClass) ?
+        '' : 'value must be an HTMLDivElement';
+    },
+    options: {
+      backgroundColor(val) {
+        const msg = [
+          'value must be a full (6 char long) hex string,',
+          'e.g. #ffffff, not #fff',
+        ].join(' ');
+        if (typeof val !== 'string') {
+          return msg;
+        }
+        return /^#[0-9A-F]{6}$/i.test(val) ? '' : msg;
+      },
+    },
   };
+}
+
+function generateWrongNumberOfArgumentsErrorReturnValue() {
+  return [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'The new XCharts() constructor expects a single argument',
+  ].join('\n');
+}
+
+function generateNotObjectArgumentErrorReturnValue() {
+  return [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'Argument to the new XCharts() constructor must be an object',
+    'e.g. {  }, Object.create(null)',
+  ].join('\n');
 }
