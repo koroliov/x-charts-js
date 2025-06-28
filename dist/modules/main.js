ea08c7e08450b9aa83a0ca2866c1bb8c9ff54165
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-28 17:18:40 +0300

    Do Flow build

--- dist/modules/main.js
+++ dist/modules/main.js
@@ -74,7 +74,7 @@ export default class XCharts {
 
     function doValidation(constructorArguments              ) 
                                  {
-      const dict = getValidationDictionaryForContructorArgument();
+      const dict = getValidationDictionaryForContructorArgument(HTMLDivElement);
       const errorMsg = validateConstructorArgument(dict, constructorArguments);
       if (errorMsg) {
         that._attemptToShowError(errorMsg);
