83235dba248bc131347505bbf0baca087627b604
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-28 07:21:01 +0300

    Move logic from pie-3d add() validation
    The changes need thorough refactoring, currently the tests pass.

--- src/components/pie-3d/validation/add-method-arg.js
+++ src/components/pie-3d/validation/add-method-arg.js
@@ -1,6 +1,8 @@
 //@flow strict
 import type { ValidationDictionary, ComponentDatum, } from '../../../types.js';
 import { isObject, } from '../../../utils/validation.js';
+import { validate as validateByDictionary, } from
+  '../../../validation/by-dictionary.js';
 import { getDictionary as getDictionaryCommon, }
   from '../../../validation/add-method-arg.js';
 
@@ -9,10 +11,15 @@ export function validate(
   dict: ValidationDictionary,
   userProvidedArg: { [string]: mixed, }
 ): string {
-  const propsToCheckSet = getPropsToCheckSet();
-  const mapperPropsArray = Object.keys(dict);
   let errMsg = '';
-  if (errMsg = handleStructureAndPrimitiveValues()) {
+  if (errMsg = validateByDictionary({
+    errorCode: 'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG',
+    topLevelPropName: 'pie-3d',
+    ignorePropsSet:
+      new Set(Object.keys(getDictionaryCommon())),
+    dictionary: dict,
+    value: userProvidedArg,
+  })) {
     return errMsg;
   } else if (errMsg = validateTotalValue()) {
     return errMsg;
@@ -21,7 +28,7 @@ export function validate(
 
   function validateTotalValue(): string {
     const total = userProvidedArg.data
-      //After the handleStructureAndPrimitiveValues() call this is guaranteed to
+      //After the validateByDictionary() call this is guaranteed to
       //be an array of ComponentDatum
       //$FlowFixMe[incompatible-use]
       .reduce((t: number, d: ComponentDatum) => t + d.value, 0);
@@ -30,253 +37,6 @@ export function validate(
       'Component pie-3d -> data:',
       '  total value must be >= 0',
     ].join('\n');
-  }
-
-  function handleStructureAndPrimitiveValues(): string {
-    let propNameForErrMsg = 'pie-3d';
-    let userProvidedDataToCheck = userProvidedArg;
-    let propsToCheckArray = Array.from(propsToCheckSet);
-    let mapper = dict;
-    let mapperPropsSet = new Set(mapperPropsArray);
-    let i = 0;
-    let isArrayInProcess = false;
-    const stack: Array<{
-      propNameForErrMsg: string,
-      userProvidedDataToCheck: { [string]: mixed, },
-      propsToCheckArray: Array<string>,
-      mapperPropsSet: Set<string>,
-      mapper: ValidationDictionary,
-      i: number,
-      isArrayInProcess: boolean,
-    }> = [];
-
-    for (; i < propsToCheckArray.length; i++) {
-      const propOnMapper = isArrayInProcess ? '0' : propsToCheckArray[i];
-      const propOnArg = isArrayInProcess ? String(i) : propsToCheckArray[i];
-      let errMsg = '';
-      if (errMsg = checkUnknownOrUnexpectedProperty(propOnMapper, propOnArg)) {
-        return errMsg;
-      } else if (errMsg = handleExpectedProperty(propOnMapper, propOnArg)) {
-        return errMsg;
-      } else if (errMsg = moveUpIfFinishedOnThisLevel()) {
-        return errMsg;
-      }
-    }
-    if (mapperPropsSet.size > 0) {
-      return getMissingPropErrorMsg();
-    }
-    return '';
-
-    function moveUpIfFinishedOnThisLevel() {
-      if (i === propsToCheckArray.length - 1) {
-        do {
-          if (mapperPropsSet.size > 0) {
-            return getMissingPropErrorMsg();
-          }
-          if (stack.length) {
-            popStack();
-          }
-          if (i === propsToCheckArray.length - 1 && mapperPropsSet.size > 0) {
-            return getMissingPropErrorMsg();
-          }
-        } while (stack.length && i === propsToCheckArray.length - 1);
-      }
-      return '';
-    }
-
-    function handleExpectedProperty(propOnMapper: string, propOnArg: string) {
-      let msg = '';
-      if (isObject(mapper[propOnMapper])) {
-        msg = enterObjectInMapper();
-      } else if (Array.isArray(mapper[propOnMapper])) {
-        msg = enterArrayInMapper();
-      } else {
-        msg = handleFunctionInMapper();
-      }
-      return msg;
-
-      function enterObjectInMapper() {
-        stack.push({
-          propNameForErrMsg,
-          userProvidedDataToCheck,
-          propsToCheckArray,
-          mapperPropsSet,
-          mapper,
-          i,
-          isArrayInProcess,
-        });
-        propNameForErrMsg = propOnArg;
-        if (!isObject(userProvidedDataToCheck[propOnMapper])) {
-          return getNonObjectErrorMsg();
-        }
-        isArrayInProcess = false;
-        //After the above isObject() call it is guaranteed to be an object
-        //$FlowFixMe[incompatible-type]
-        userProvidedDataToCheck = userProvidedDataToCheck[propOnArg];
-        //It's okay to treat ValidationDictionaryPure as ValidationDictionary here,
-        //because ValidationDictionaryPure is a subtype of ValidationDictionary and it's
-        //supposed to fit whereever a ValidationDictionary fits.
-        //$FlowFixMe[incompatible-type]
-        //$FlowFixMe[incompatible-function-indexer]
-        mapper = mapper[propOnMapper];
-        //After the above isObject() call it is guaranteed to be an object
-        //$FlowFixMe[not-an-object]
-        //$FlowFixMe[incompatible-type]
-        propsToCheckArray = Object.keys(userProvidedDataToCheck);
-        mapperPropsSet = new Set(Object.keys(mapper));
-        i = -1;
-        return '';
-      }
-
-      function enterArrayInMapper() {
-        stack.push({
-          propNameForErrMsg,
-          userProvidedDataToCheck,
-          propsToCheckArray,
-          mapperPropsSet,
-          mapper,
-          i,
-          isArrayInProcess,
-        });
-        propNameForErrMsg = propOnArg;
-        if (!(Array.isArray(userProvidedDataToCheck[propOnArg]) &&
-          userProvidedDataToCheck[propOnArg].length)) {
-          const nestedPropPath = getPropNestedPath();
-          return [
-            'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
-            `Component ${ nestedPropPath }:`,
-            '  must be an non empty array',
-          ].join('\n');
-        }
-        isArrayInProcess = true;
-        //After the above Array.isArray() call it is guaranteed to be a non
-        //empty array, also it's been decided in this validation logic to treat
-        //an array as an object with string, integer properties: '0' etc.
-        //$FlowFixMe[incompatible-type]
-        userProvidedDataToCheck = userProvidedDataToCheck[propOnArg];
-        //It's okay to treat ValidationDictionaryPure as ValidationDictionary here,
-        //because ValidationDictionaryPure is a subtype of ValidationDictionary and it's
-        //supposed to fit whereever a ValidationDictionary fits.
-        //$FlowFixMe[incompatible-type]
-        //$FlowFixMe[incompatible-function-indexer]
-        mapper = mapper[propOnMapper];
-        propsToCheckArray = getPropsToCheckArray();
-        mapperPropsSet = new Set(propsToCheckArray);
-        i = -1;
-        return '';
-
-        function getPropsToCheckArray() {
-          const props = [];
-          //It's checked above that it's an array
-          //$FlowFixMe[invalid-compare]
-          for (let i = 0; i < userProvidedDataToCheck.length; i++) {
-            props.push(String(i));
-          }
-          return props;
-        }
-      }
-
-      function handleFunctionInMapper() {
-        //The type ValidationDictionary says that if it's prop is not an Array and
-        //not an Object, then it's a function with a particular signature. Flow
-        //is not able to detect it yet.
-        //$FlowFixMe[prop-missing]
-        //$FlowFixMe[not-a-function]
-        const msg = mapper[isArrayInProcess ?
-          '0' : propOnMapper](userProvidedDataToCheck[propOnArg]);
-        if (msg) {
-          const nestedPropPath = getPropNestedPath();
-          const pathEnding = nestedPropPath.length ?
-            ` -> ${ propOnMapper }` : '';
-          return [
-            'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
-            `Component ${ nestedPropPath }${ pathEnding }:`,
-            `  ${ msg }`,
-          ].join('\n');
-        }
-        mapperPropsSet.delete(propOnMapper);
-        return '';
-      }
-    }
-
-    function checkUnknownOrUnexpectedProperty(
-      propOnMapper: string,
-      propOnArg: string) {
-      if (isArrayInProcess) {
-        if (isNonObjectInArrayError(propOnMapper, propOnArg)) {
-          return getNonObjectErrorMsg(propOnArg);
-        }
-      } else {
-        if (!mapperPropsSet.has(propOnArg)) {
-          return getUnknownPropertyError(propOnArg);
-        }
-      }
-      return '';
-    }
-
-    function popStack() {
-      //With the .length check it's guaranteed not to be undefined
-      //$FlowFixMe[incompatible-type]
-      const popped: (typeof stack)[0] = stack.pop();
-      propNameForErrMsg = popped.propNameForErrMsg;
-      userProvidedDataToCheck = popped.userProvidedDataToCheck;
-      propsToCheckArray = popped.propsToCheckArray;
-      mapperPropsSet = popped.mapperPropsSet;
-      mapper = popped.mapper;
-      i = popped.i;
-      isArrayInProcess = popped.isArrayInProcess;
-      mapperPropsSet.delete(propsToCheckArray[i]);
-    }
-
-    function getMissingPropErrorMsg() {
-      const nestedPropPath = getPropNestedPath();
-      return [
-        'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
-        `Component ${ nestedPropPath }:`,
-        `  missing properties: ${
-          Array.from(mapperPropsSet).join(', ') }`,
-      ].join('\n');
-    }
-
-    function getUnknownPropertyError(propOnArg: string) {
-      const nestedPropPath = getPropNestedPath();
-      return [
-        'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
-        `Component ${ nestedPropPath }:`,
-        `  unknown property '${ propOnArg }'`,
-      ].join('\n');
-    }
-
-    function isNonObjectInArrayError(propOnMapper: string, propOnArg: string) {
-      return isObject(mapper[propOnMapper]) &&
-        !isObject(userProvidedDataToCheck[propOnArg]);
-    }
-
-    function getNonObjectErrorMsg(propOnArg?: string) {
-      const nestedPropPath = getPropNestedPath();
-      const pathEnding = propOnArg?.length ? ` -> ${ propOnArg }` : '';
-      return [
-        'ERR_X_CHARTS_INVALID_ADD_METHOD_ARG:',
-        `Component ${ nestedPropPath }${ pathEnding}:`,
-        '  must be an object',
-      ].join('\n');
-    }
-
-    function getPropNestedPath() {
-      let prevPath = stack.map(i => i.propNameForErrMsg).join(' -> ');
-      if (prevPath.length) {
-        prevPath += ' -> ';
-      }
-      return `${ prevPath }${ propNameForErrMsg }`;
-    }
-  }
-
-  function getPropsToCheckSet() {
-    const propsInArg = new Set(Object.keys(userProvidedArg));
-    const propsCheckedInCommonValidation = new
-      Set(Object.keys(getDictionaryCommon()));
-    propsCheckedInCommonValidation.forEach((p) => propsInArg.delete(p));
-    return propsInArg;
   }
 }
 
