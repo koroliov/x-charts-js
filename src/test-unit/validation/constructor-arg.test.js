7ddb97ba200404f9e17e0c78cd601d2925d4af55
Dmitry Koroliov <d.koroliov@gmail.com>
2025-06-28 17:16:53 +0300

    Finish tests for validation module
    The tests are not exhaustive, created them intuitively and to lay down
    the structure for the auto tests for the added module. If something is
    broken, tests will be added.

--- src/test-unit/validation/constructor-arg.test.js
+++ src/test-unit/validation/constructor-arg.test.js
@@ -82,3 +82,54 @@ tp.test('argument is not object', (t) => {
   t.deepEqual(actual, expected);
   t.end();
 });
+
+tp.test('containerDiv is not valid', (t) => {
+  const ContainerDiv = class ContainerDiv {  }
+  const ContainerSpan = class ContainerSpan {  }
+  const constructorArg = [
+    {
+      containerDiv: new ContainerSpan(),
+      options: {
+        backgroundColor: '#ffffff' /* white */,
+      },
+    },
+  ];
+  //In tests it's acceptable
+  //$FlowFixMe[incompatible-call]
+  const dict = getDictionary(ContainerDiv);
+  const expected = [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'new XCharts() argument -> containerDiv:',
+    '  value must be an HTMLDivElement',
+  ].join('\n');
+
+  const actual = validate(dict, constructorArg);
+  t.deepEqual(actual, expected);
+  t.end();
+});
+
+//options validation
+tp.test('extra argument in options', (t) => {
+  const ContainerDiv = class ContainerDiv {  }
+  const constructorArg = [
+    {
+      containerDiv: new ContainerDiv(),
+      options: {
+        foo: 'foo',
+        backgroundColor: '#ffffff' /* white */,
+      },
+    },
+  ];
+  //In tests it's acceptable
+  //$FlowFixMe[incompatible-call]
+  const dict = getDictionary(ContainerDiv);
+  const expected = [
+    'ERR_X_CHARTS_INVALID_CONSTRUCTOR_ARG:',
+    'new XCharts() argument -> options:',
+    "  unknown property 'foo'",
+  ].join('\n');
+
+  const actual = validate(dict, constructorArg);
+  t.deepEqual(actual, expected);
+  t.end();
+});
