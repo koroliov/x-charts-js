cef4f17a2c0c36e3e73d5516ae363ccad70c71fb
Dmitry Koroliov <d.koroliov@gmail.com>
2025-09-20 22:23:22 +0300

    Remove almost everything from footer in docs
    There are not that many things to show in the footer, specifically
    couldn't find a proper name for the Docs section in the footer. I's
    about to have a page titled Docs, which would contain everything:
    general notes + API. So how would it look in the footer:
    
    Docs
      Docs
    
    So I decided just to remove almost everything from the footer, except
    for the copyright note.

--- docs-src/var/docusaurus.config.js
+++ docs-src/var/docusaurus.config.js
@@ -118,15 +118,15 @@ const config = {
       footer: {
         style: 'dark',
         links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
+          //{
+          //  title: 'Docs',
+          //  items: [
+          //    {
+          //      label: 'Tutorial',
+          //      to: '/docs/intro',
+          //    },
+          //  ],
+          //},
           //{
           //  title: 'Community',
           //  items: [
@@ -144,19 +144,19 @@ const config = {
           //    //},
           //  ],
           //},
-          {
-            title: 'More',
-            items: [
-              //{
-              //  label: 'Blog',
-              //  to: '/blog',
-              //},
-              {
-                label: 'GitHub',
-                href: 'https://github.com/koroliov/x-charts-js',
-              },
-            ],
-          },
+          //{
+          //  title: 'More',
+          //  items: [
+          //    //{
+          //    //  label: 'Blog',
+          //    //  to: '/blog',
+          //    //},
+          //    {
+          //      label: 'GitHub',
+          //      href: 'https://github.com/koroliov/x-charts-js',
+          //    },
+          //  ],
+          //},
         ],
         copyright: `Copyright © ${new Date().getFullYear()} koroliov. Built with Docusaurus.`,
       },
