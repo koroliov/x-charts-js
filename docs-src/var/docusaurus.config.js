360a13a97156d7e4c61c32e9ca0008bc203f890c
Dmitry Koroliov <d.koroliov@gmail.com>
2025-09-28 12:19:54 +0300

    Throw onBrokenMarkdownLinks in Docusaurus

--- docs-src/var/docusaurus.config.js
+++ docs-src/var/docusaurus.config.js
@@ -31,7 +31,7 @@ const config = {
   projectName: 'x-charts-js', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
